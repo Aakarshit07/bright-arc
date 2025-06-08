6817529918eb33671eedd1ec7c3ebee0f307ccd6
aakarshit choudhary <aakarshit070@gmail.com>
2025-06-09 00:43:54 +0530

    fix faq section

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,11 +1,13 @@
 import { ContactForm } from "@/components/contact-us/contact-form";
+import FAQSection from "@/components/faqs/faq-section";
 import { DefaultLayout } from "@/components/layout/default-layout";
 
 export default function AboutPage() {
   return (
     <DefaultLayout>
         <div className="flex flex-col gap-[70px] md:gap-[106px]">
-            <ContactForm/>
+          <ContactForm/>
+          <FAQSection/>
         </div>
     </DefaultLayout>
   )
