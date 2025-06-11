17f871af77678ecdca4acdec82f38c779952c150
aakarshit choudhary <aakarshit070@gmail.com>
2025-06-11 16:55:59 +0530

    fix journey section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,33 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Poppins } from "next/font/google"
-import "./globals.css"
-import Header from "@/components/shared/header";
-import { Footer } from "@/components/shared/footer";
-import { Toaster } from "@/components/ui/sonner"
+import type React from "react";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-poppins',
-  display: 'swap',
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
   title: "Bright Arc",
   description:
     "Bright Arc is a web application that provides a platform for students to connect with their academic advisors and explore various resources related to their academic pursuits.",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" >
-      <body className={`${poppins.variable} font-poppins`}> 
+    <html lang="en">
+      <body className={`${poppins.variable} font-poppins`}>
         {children}
         <Toaster />
       </body>
     </html>
-  )
+  );
 }
